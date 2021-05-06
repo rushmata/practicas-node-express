8212c2a8d383a43c6d88a05e806fb629fd4bdab1
DIR\fernando.mata.pelaez <fernando.mata.pelaez@accenture.com>
2021-05-06 11:54:04 +0200

    Removing none needed files

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-// Cargamos el modulo express
-const express = require('express');
-const router = express.Router();
-// Cargamos el controlador del usuario
-const userController = require('../app/api/controllers/users');
-// Especificamos nuestras rutas teniendo en cuenta los metodos creados en nuestro controlador, y especificando que seran rutas que usaran el metodo POST
-router.post('/register', userController.create);
-router.post('/authenticate', userController.authenticate);
-module.exports = router;
